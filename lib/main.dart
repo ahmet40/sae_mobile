f595bde0345c7a6f5e5a8be09d88edc98de5c653
ahmet40 <ahmet.baba@etu.univ-orleans.fr>
2024-04-07 16:29:37 +0200

    changement sur supabase

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'modele/modele_local/bd/database.dart';
 import 'modele/modele_supabase/bd/utilistaeurService.dart';
 import './page/connexion.dart';
 
-const supabaseurl = 'https://ivqvbkryvyegommrevcf.supabase.co';
+const supabaseurl = 'https://kucvmvquijdxvngmgdjb.supabase.co';
 const apikey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2cXZia3J5dnllZ29tbXJldmNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE2MjA2MDUsImV4cCI6MjAyNzE5NjYwNX0.yjdkuxy3_aa_cu63s37g9HWrVg0RDESciv0NQsV4gsU';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1Y3ZtdnF1aWpkeHZuZ21nZGpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI0OTkwMjYsImV4cCI6MjAyODA3NTAyNn0.imnxBVUYFsbp5DVINZiH9grqFlPIbUWc_dA7trfmtLk';
 
 final supabase = Supabase.instance.client;
 Future<void> main() async {
